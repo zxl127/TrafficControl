30d768d1ce44faad5db1f05250d4f61a559e85a5
zhouxiangli <zhouxiangli@sztozed.net>
2018-06-20 13:56:51 +0800

    GNU GPL license

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/****************************************************************************
+**
+** Copyright (C) 2018 The * Company Ltd.
+** Contact: 1150705943@qq.com
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
